64cd38bf3c7ea31752d1d66e9b1c021af798e9f3
cclcr21 <cclcr21@rit.edu>
2021-09-01 13:44:13 -0400

    + material-ui / top nav implem

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { Switch, Route, HashRouter, Link } from 'react-router-dom';
-// import icon from '../../assets/icon.svg';
+import { AppBar, Tab } from '@material-ui/core';
 import './App.global.css';
+import { TabContext, TabList, TabPanel } from '@material-ui/lab';
 import Settings from './Settings';
 import Home from './Home';
 
 export default function App() {
+  const [value, setValue] = React.useState('0');
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleChange = (_event: React.ChangeEvent<{}>, newValue: string) => {
+    // Update key for our current TabPanel
+    setValue(newValue);
+  };
+
   return (
-    <HashRouter>
-      <Link to="/">
-        <h2>Home</h2>
-      </Link>
-      <Link to="/settings">
-        <h2>Settings</h2>
-      </Link>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/settings" component={Settings} />
-      </Switch>
-    </HashRouter>
+    <div>
+      <TabContext value={value}>
+        <AppBar position="fixed">
+          <TabList
+            onChange={handleChange}
+            aria-label="simple tabs example"
+            centered
+          >
+            <Tab label="Home" value="0" />
+            <Tab label="Settings" value="1" />
+          </TabList>
+        </AppBar>
+        <TabPanel value="0">
+          <Home />
+        </TabPanel>
+        <TabPanel value="1">
+          <Settings />
+        </TabPanel>
+      </TabContext>
+    </div>
   );
 }
